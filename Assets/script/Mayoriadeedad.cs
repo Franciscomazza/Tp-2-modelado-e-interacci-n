9c0959360fa64223c0186e4db4b44511bbffd1fa
MAZZA FRANCISCO <46990260@ort>
2022-04-04 23:58:43 -0300

    Add video 2 y 3 first part and create video 4 5 6 second part

---
+++ Assets/script/Mayoriadeedad.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Mayoriadeedad : MonoBehaviour {
+
+   public int edadUsuario;
+    public Text mitexto;
+
+
+
+    void Start() {
+
+       
+
+        if(edadUsuario >= 18)
+        {
+            mitexto.text = "Es Mayor de edad";
+            Debug.Log("es mayor de edad");
+        }
+        else
+        {
+            mitexto.text = "no es mayor de edad";
+            Debug.Log(" No es mayor de edad");
+        }
+
+    }
+
+}
---
+++ Assets/video456/Validarcontraseña.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Validarcontraseña : MonoBehaviour
+{
+
+    string contraseniaCorrecta;
+    string contraseniaUsuario;
+    public Text ingresoUsuario;
+    public GameObject cartelitoMsj;
+    public Text textoMjs;
+    
+    // Start is called before the first frame update
+    void Start(){
+        contraseniaCorrecta = "12345";
+        cartelitoMsj.SetActive(false);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+       
+    }
+
+   
+    public void Validarcontrasenia()
+    {
+        contraseniaUsuario = ingresoUsuario.text;
+        if (contraseniaUsuario == contraseniaCorrecta)
+        {
+            cartelitoMsj.SetActive(true);
+            textoMjs.text = "bienvenido";
+            Debug.Log ("Bienvenido");
+        }
+        else
+        {
+            cartelitoMsj.SetActive(true);
+            textoMjs.text = "contraseña Incorrecta";
+            Debug.Log("Contraseña Incorrecta");
+
+
+        }
+
+
+
+    }
+
+}
